1205facaeec88714e73ff3ff343a3ec68c8e33c0
AlexLeoTW <alex@alexleo.click>
2019-07-12 19:17:51 +0800

    add CNN MNIST

---
+++ performance analysis/Keras_CNN_MNIST.py
@@ -0,0 +1,73 @@
+'''Loosely Based on Mnist CNN by keras-team
+https://keras.io/examples/mnist_cnn/
+https://github.com/keras-team/keras/blob/master/examples/mnist_cnn.py
+'''
+
+import sys
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import cmdargv
+
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+# read commandline arguments
+options = cmdargv.parse_argv(sys.argv)
+print('options = {}'.format(options))
+
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(options.conv1[0], kernel_size=(options.conv1[1], options.conv1[1]),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(options.conv2, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(options.pool, options.pool)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(options.dense, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
---
+++ performance analysis/cmdargv.py
@@ -0,0 +1,24 @@
+import argparse, sys, os
+
+def parse_argv(argv):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-conv1', help='configure first Conv2D layer ex. 32 2',
+        nargs='+', type=int, metavar=('filters', 'kernel_size'), default=[32, 2])
+    parser.add_argument('-conv2', help='configure second Conv2D layer ex. 64',
+        type=int, metavar='filters', default=64)
+    parser.add_argument('-pool', help='configure MaxPooling2D pool_size ex. 2',
+        type=int, metavar='pool_size', default=2)
+    parser.add_argument('-dense', help='configure units of dense layer ex. 128',
+        type=int, metavar='units', default=128)
+
+    args = parser.parse_args()
+
+    return args
+
+def main(argv):
+    options = parse_argv(argv)
+    print(options)
+
+if __name__ == '__main__':
+    main(sys.argv)
