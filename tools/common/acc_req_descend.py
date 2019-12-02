35e55ac98d23bd9f8c263569b75b00689448d7fc
AlexLeoTW <alex@alexleo.click>
2019-12-02 18:49:10 +0800

    move draw_decending_acc_requirement() to common

--- tools/common/acc_req_descend.py
+++ tools/common/acc_req_descend.py
@@ -1,5 +1,6 @@
 import random
 import string
+import numpy as np
 from scipy.optimize import fsolve
 
 
@@ -29,7 +30,8 @@ def find_first_hit(df, params):
     col_name = '{}_pass'.format(_randomString(5))
 
     # insert a new column named XXXXX_pass indicates a hit
-    df.insert(loc=len(params), column=col_name, value=df['val_acc'] >= acc_requirement(df['end_time']))
+    df.insert(loc=len(params), column=col_name,
+              value=df['val_acc'] >= acc_requirement(df['end_time']))
 
     # keep the "first" hit record, drop the others
     first_hit = df[df[col_name]]
@@ -53,3 +55,19 @@ def find_first_hit(df, params):
     first_hit.drop(columns=[col_name], inplace=True)
 
     return first_hit
+
+
+def draw_decending_acc_requirement(plt, **kwargs):
+    x_ticks = plt.xticks()[0]
+    x_min, x_max = max(min(x_ticks), 0), max(x_ticks)
+    y_ticks = plt.yticks()[0]
+    y_min, y_max = min(y_ticks), max(y_ticks)
+
+    xs = np.arange(x_min, x_max, 5)
+    ys = acc_requirement(xs)
+
+    xs = xs[np.logical_and(ys > y_min, ys < y_max)]
+    ys = ys[np.logical_and(ys > y_min, ys < y_max)]
+
+    default_plot_arg = {'c': '#303030', 'linestyle': '--', 'linewidth': '2'}
+    plt.plot(xs, ys, **{**default_plot_arg, **kwargs})
--- tools/first_hit/first_hit.py
+++ tools/first_hit/first_hit.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from statistics import Statistics
 from auto_params import auto_params
+import acc_req_descend
 
 highlighted_colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b']
 dimmed_colors = ['#3B5F77', '#AF7C4E', '#467546', '#803A3A', '#776587', '#67514D']
@@ -10,46 +11,28 @@ figsize = [30, 16]
 
 options = auto_params()
 statistics = Statistics(options.path, options.params)
+collected_avg = statistics.deep_collect(['val_acc', 'end_time'], avg=False)
+first_hits = acc_req_descend.find_first_hit(collected_avg, options.params)
+
 plt.figure(figsize=figsize)
 
-# collect avg-ed data
-collected_avg = statistics.deep_collect(['val_acc', 'end_time'], avg=True)
-
-
-def acc_requirement(x):
-    return (-0.00003) * x + 1
-
-
-def draw_decending_acc_requirement(f):
-    xs = np.arange(0.0, 1000, 5)
-    ys = f(xs)
-    plt.plot(xs, ys, c='#303030', linestyle='--', linewidth='2')
-
-
-def find_first_hit(df, params):
-    df.insert(loc=len(params), column='pass', value=df['val_acc'] >= acc_requirement(df['end_time']))
-
-    first_hit = df[df['pass']]
-    first_hit = first_hit.drop_duplicates(subset=params)
-    return first_hit
-
-
 for param in options.params:
-
     print('param = {param}'.format(param=param))
-    draw_decending_acc_requirement(f=acc_requirement)
     highlighted_colors_iter = iter(highlighted_colors)
     dimmed_colors_iter = iter(dimmed_colors)
 
     for param_value in collected_avg[param].drop_duplicates().values:
 
         selected = collected_avg[collected_avg[param] == param_value]
+        sel_hit = first_hits[first_hits[param] == param_value]
+
         plt.scatter(selected['end_time'], selected['val_acc'],
             alpha=0.5, edgecolors='none', c=next(dimmed_colors_iter))
 
-        first_hit = find_first_hit(selected, options.params)
-        plt.scatter(first_hit['end_time'], first_hit['val_acc'], [120]*first_hit.shape[0],
+        plt.scatter(sel_hit['end_time'], sel_hit['val_acc'], [120]*first_hits.shape[0],
             label=str(param_value), c=next(highlighted_colors_iter), edgecolors='black', marker='x')
+
+    acc_req_descend.draw_decending_acc_requirement(plt)
 
     plt.xlabel("end_time(s)")
     plt.ylabel("val_acc(%)")
